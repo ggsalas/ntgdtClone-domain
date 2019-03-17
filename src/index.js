6f2c7cd8bebc76813e051fede59f6057e2a8baed
Gabriel Salas <ggsalas@gmail.com>
2019-03-17 20:16:59 +0100

    feat(Root): create entry point

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('hello world')
+import {EntryPointFactory} from '@s-ui/domain'
+import UseCasesMemesFactory from './memes/UseCases/factory'
+
+export default EntryPointFactory({
+  config: {},
+  useCases: {getListMemesUseCase: UseCasesMemesFactory.getListMemesUseCase()}
+})
